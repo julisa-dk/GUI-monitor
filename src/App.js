5c1862a1b2e9a7d1384f2015f91b5e94041ea7cb
julisa-dk <36698832+julisa-dk@users.noreply.github.com>
2022-02-23 15:10:32 +0100

    alljobs connect to api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./styles.css";
+import Alljobs from "./components/Alljobs";
+
 
 export default function App() {
   return (
     <div className="App">
-      
+      <h1>All jobs</h1>
+      <Alljobs />
     </div>
   );
 }
